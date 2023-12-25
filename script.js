0a29e12262ddb394f6d5cbf1bd8edab9e8051445
Michaeledward26 <154841009+Michaeledward26@users.noreply.github.com>
2023-12-26 06:26:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const audio =
+
+docamentbqderySdbeament!audyo');
+
+audio.addEventListener('play', () => { body.classList.add('play-music'); });
+
+audio.addEventListener('pause', () => { body.classList.remove('play-music'); });
